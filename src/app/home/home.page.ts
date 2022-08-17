811f27b30d8e83ae6e2b152abe1e36349364affa
DavidSanchez <luigisperstar7@gmail.com>
2022-08-17 19:22:35 -0400

    ahora se hace el hash256 de la password

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpClientModule} from '@angular/common/http';
 import {headersJSON, serverURL} from '../../global-variables';
+import * as shajs from 'sha.js';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -9,11 +11,13 @@ import {headersJSON, serverURL} from '../../global-variables';
 export class HomePage {
   constructor(private http: HttpClient) {}
 ingresar(user: string,password: string){
+  const hashedPassword: string = shajs('sha256').update(password).digest('hex');
+  console.log(hashedPassword);
   const usuario = {
     user,
-    password
+    password: hashedPassword
   };
-  this.http.post(serverURL + '/register', JSON.stringify(usuario),{headers:headersJSON}).subscribe({next: (data) => {
+  this.http.post(serverURL + '/login', JSON.stringify(usuario),{headers:headersJSON}).subscribe({next: (data) => {
     alert('llego el resultado' + data);
   },
   error: (error) => {
